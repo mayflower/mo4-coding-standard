3fb5152f79d732bb18c95aab265cda2b8267c2ec
Xaver Loppenstedt <xaver@loppenstedt.de>
2013-10-31 21:23:49 +0100

    added Alphabetical Use Statements Sniff

---
+++ Sniffs/Formatting/AlphabeticalUseStatementsSniff.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of the mo4-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-MO4
+ * @author   Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/Mayflower/mo4-coding-standard
+ */
+
+/**
+ * Alphabetical Use Statements sniff.
+ *
+ * Use statements must be in alphabetical order, grouped by empty lines
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer-MO4
+ * @author    Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @copyright 2013 Xaver Loppenstedt, some rights reserved.
+ * @license   http://spdx.org/licenses/MIT MIT License
+ * @link      https://github.com/Mayflower/mo4-coding-standard
+ */
+class MO4_Sniffs_Formatting_AlphabeticalUseStatementsSniff
+    extends PSR2_Sniffs_Namespaces_UseDeclarationSniff
+{
+    /**
+     * last use statement seen in group
+     *
+     * @var string
+     */
+    private $_lastUseStatement = '';
+
+    /**
+     * line number of the last seen use statement
+     *
+     * @var int
+     */
+    private $_lastLine = -1;
+
+    /**
+     * current file
+     *
+     * @var string
+     */
+    private $_currentFile = null;
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        parent::process($phpcsFile, $stackPtr);
+
+        if ($this->_currentFile !== $phpcsFile->getFilename()) {
+            $this->_lastLine = -1;
+            $this->_lastUseStatement = '';
+            $this->_currentFile = $phpcsFile->getFilename();
+        }
+
+        $tokens = $phpcsFile->getTokens();
+        $line   = $tokens[$stackPtr]['line'];
+        $start  = $phpcsFile->findNext(T_STRING, $stackPtr + 1);
+        $end    = $phpcsFile->findNext([T_AS, T_SEMICOLON, T_COMMA], $stackPtr + 1);
+
+        $currentUseStatement = $phpcsFile->getTokensAsString($start, $end - $start);
+
+        if (($this->_lastLine + 1) < $line) {
+            $this->_lastLine         = $line;
+            $this->_lastUseStatement = $currentUseStatement;
+
+            return;
+        }
+
+        if ($this->_lastUseStatement !== '') {
+            if (strcmp($this->_lastUseStatement, $currentUseStatement) > 0) {
+                $msg = 'USE statements mus be sorted alphabetically';
+
+                $phpcsFile->addError($msg, $stackPtr);
+            }
+        }
+
+        $this->_lastUseStatement = $currentUseStatement;
+        $this->_lastLine         = $line;
+    }
+}
+ 
---
+++ Tests/Formatting/AlphabeticalUseStatementsUnitTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of the mo4-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-MO4
+ * @author   Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/Mayflower/mo4-coding-standard
+ */
+
+/**
+ * Unit test class for the AlphabeticalUseStatements sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer-MO4
+ * @author    Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @copyright 2013 Xaver Loppenstedt, some rights reserved.
+ * @license   http://spdx.org/licenses/MIT MIT License
+ * @link      https://github.com/Mayflower/mo4-coding-standard
+ */
+class MO4_Tests_Formatting_AlphabeticalUseStatementsUnitTest
+    extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile test file
+     *
+     * @return array(int => int)
+     */
+    protected function getErrorList($testFile = '')
+    {
+        switch ($testFile) {
+        case 'AlphabeticalUseStatementsUnitTest.pass.inc':
+            return array();
+        case 'AlphabeticalUseStatementsUnitTest.fail.inc':
+            return array(
+                4  => 1,
+                5  => 1,
+                8  => 1,
+                9  => 1,
+                12 => 1,
+            );
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    protected function getWarningList()
+    {
+        return array();
+    }
+}
+ 
