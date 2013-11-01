0847329542347ea13714e8387ae09fb88f33e2dc
Xaver Loppenstedt <xaver@loppenstedt.de>
2013-11-01 16:16:44 +0100

    use array short tag sniff

---
+++ Sniffs/Formatting/UseArrayShortTagSniff.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the mo4-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-MO4
+ * @author   Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/Mayflower/mo4-coding-standard
+ */
+
+/**
+ * Use Array Short Tag sniff.
+ *
+ * Use the array short tag [...] instead of array(...)
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer-MO4
+ * @author    Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @copyright 2013 Xaver Loppenstedt, some rights reserved.
+ * @license   http://spdx.org/licenses/MIT MIT License
+ * @link      https://github.com/Mayflower/mo4-coding-standard
+ */
+class MO4_Sniffs_Formatting_UseArrayShortTagSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * Registers the tokens that this sniff wants to listen for.
+     *
+     * @return array(int)
+     * @see    Tokens.php
+     */
+    public function register()
+    {
+        return array(T_ARRAY);
+    }
+
+    /**
+     * Called when one of the token types that this sniff is listening for
+     * is found.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The PHP_CodeSniffer file where the
+     *                                        token was found.
+     * @param int                  $stackPtr  The position in the PHP_CodeSniffer
+     *                                        file's token stack where the token
+     *                                        was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $phpcsFile->addError('Array short tag [ ... ] must be used', $stackPtr);
+    }
+}
+ 
