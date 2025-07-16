f220f009de5f3684f1217094c081edb2e8cf4b22
Michael Moll <kvedulv@kvedulv.de>
2025-07-16 14:17:45 +0200

    separate static analysis bootstrap

---
+++ tests/static_analysis_bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the mo4-coding-standard (phpcs standard)
+ *
+ * Bootstrap file for PHP_CodeSniffer MO4 Coding Standard unit tests.
+ *
+ * @author  Xaver Loppenstedt <xaver@loppenstedt.de>
+ *
+ * @license http://spdx.org/licenses/MIT MIT License
+ *
+ * @link    https://github.com/mayflower/mo4-coding-standard
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/squizlabs/php_codesniffer/autoload.php';
+require_once __DIR__.'/../vendor/squizlabs/php_codesniffer/src/Util/Tokens.php';
